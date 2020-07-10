1c1aebd345461bbfcb0b2c53e91fcfa756b1ec2f
Alex Zarebski <aezarebski@gmail.com>
2020-07-10 17:18:20 +0100

    basic functionality for the popsize-distribution timing

--- examples/timing-evaluation/README.md
+++ examples/timing-evaluation/README.md
@@ -8,3 +8,9 @@ function.
 ```
 make figures
 ```
+
+Note that to generate the timing for the `popsize-distribuion` function, you
+need to clone this repository into the current directory and then set up the
+necessary python environment. Then after running the BDSCOD timing, you need to
+adjust the simulation files and run `run-python-timing.sh` from withing
+`popsize-distribution` to generate the timing results.
--- examples/timing-evaluation/src/plot-profiles.R
+++ examples/timing-evaluation/src/plot-profiles.R
@@ -1,17 +1,36 @@
 library(dplyr)
 library(ggplot2)
+library(reshape2)
 library(magrittr)
+library(jsonlite)
+
 
 x1 <- read.csv("fobber.csv")
 x2 <- read.csv("out/simulation-sizes-and-llhds.csv", header = FALSE) %>% set_names(c("Size", "Llhd", "Name"))
-x <- left_join(x1, x2, by = "Name")
+bdscod_records <- left_join(x1, x2, by = "Name")
 
 
-g <- ggplot(data = x,
-            mapping = aes(x = Size, y = Mean, ymin = Mean - 2 * Stddev, ymax = Mean + 2 * Stddev)) +
-    geom_point() +
-    geom_errorbar() +
-    geom_smooth(method = "lm")
+pop_sim_records <- list.files(path = "out/", pattern = "^popsize", full.names = TRUE) %>%
+    map(compose(as.data.frame, read_json)) %>%
+    bind_rows %>%
+    mutate(Name = gsub(pattern = ".*observations", replacement = "out/simulated-observations", x = inputJson))
+
+tmp <- pop_sim_records %>% rename(Mean2 = evaluationTime / numReplicates) %>% select(Name, Mean2)
+plot_df <- left_join(bdscod_records, tmp, by = "Name") %>% select(Size, Mean, Mean2) %>% melt(id.vars = "Size")
+rm(tmp)
+
+## g <- ggplot(data = bdscod_records,
+##             mapping = aes(x = Size, y = Mean, ymin = Mean - 2 * Stddev, ymax = Mean + 2 * Stddev)) +
+##     geom_point() +
+##     geom_errorbar() +
+##     geom_smooth(method = "lm")
+
+## ggsave("out/profiles.png", g)
+## ggsave("out/profiles.pdf", g)
+
+
+g <- ggplot(data = plot_df, mapping = aes(x = Size, y = value)) +
+    geom_point() + geom_smooth(method = "lm") + facet_wrap(~variable, scales = "free_y")
 
 ggsave("out/profiles.png", g)
 ggsave("out/profiles.pdf", g)
