d6dea835f9bf30f7ddba39dbca92d12720aa2287
aezarebski <aezarebski@gmail.com>
2020-07-12 13:04:18 +0100

    minor tweaks to handle failed calculations and explore the scaling numerically

--- examples/timing-evaluation/src/plot-profiles.R
+++ examples/timing-evaluation/src/plot-profiles.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(magrittr)
+library(purrr)
 library(jsonlite)
 
 
@@ -9,9 +10,18 @@ x1 <- read.csv("fobber.csv")
 x2 <- read.csv("out/simulation-sizes-and-llhds.csv", header = FALSE) %>% set_names(c("Size", "Llhd", "Name"))
 bdscod_records <- left_join(x1, x2, by = "Name")
 
+try_read_json <- function(json_filepath) {
+    tryCatch(expr = as.data.frame(read_json(json_filepath)),
+             error = function(e) {
+                 warning("Could not read file: ", json_filepath);
+                 NULL
+             })
+
+}
 
 pop_sim_records <- list.files(path = "out/", pattern = "^popsize", full.names = TRUE) %>%
-    map(compose(as.data.frame, read_json)) %>%
+    map(try_read_json) %>%
+    keep(compose(`!`, is.null)) %>%
     bind_rows %>%
     mutate(Name = gsub(pattern = ".*observations", replacement = "out/simulated-observations", x = inputJson))
 
@@ -19,18 +29,22 @@ tmp <- pop_sim_records %>% rename(Mean2 = evaluationTime / numReplicates) %>% se
 plot_df <- left_join(bdscod_records, tmp, by = "Name") %>% select(Size, Mean, Mean2) %>% melt(id.vars = "Size")
 rm(tmp)
 
-## g <- ggplot(data = bdscod_records,
-##             mapping = aes(x = Size, y = Mean, ymin = Mean - 2 * Stddev, ymax = Mean + 2 * Stddev)) +
-##     geom_point() +
-##     geom_errorbar() +
-##     geom_smooth(method = "lm")
-
-## ggsave("out/profiles.png", g)
-## ggsave("out/profiles.pdf", g)
-
 
 g <- ggplot(data = plot_df, mapping = aes(x = Size, y = value)) +
     geom_point() + geom_smooth(method = "lm") + facet_wrap(~variable, scales = "free_y")
 
 ggsave("out/profiles.png", g)
 ggsave("out/profiles.pdf", g)
+
+## The following commands can be used to fit a model to understand the scaling
+## of the timing of the computation.
+foo <- plot_df %>%
+    filter(variable == "Mean") %>%
+    mutate(ln_size = log(Size),
+           ln_time = log(value))
+bar <- plot_df %>%
+    filter(variable == "Mean2") %>%
+    mutate(ln_size = log(Size),
+           ln_time = log(value))
+
+lm(ln_time ~ ln_size, data = foo) %>% summary
