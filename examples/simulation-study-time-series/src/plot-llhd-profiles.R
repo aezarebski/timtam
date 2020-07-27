5a2f1b51a318ae2a7b6dd448acbae28d0893577b
Alex Zarebski <aezarebski@gmail.com>
2020-07-27 15:35:52 +0100

    include a proper comparison of the inferred and actual prevalence

--- examples/simulation-study-time-series/src/plot-llhd-profiles.R
+++ examples/simulation-study-time-series/src/plot-llhd-profiles.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(jsonlite)
 library(purrr)
+library(ape)
 
 config <- read_json("ts-config.json")
 
@@ -39,26 +40,52 @@ all_events <- read.csv("out/all-simulated-events.csv",
     set_names(c("event", "time", "primary", "secondary")) %>%
     mutate(delta = primary_count(primary)) %>%
     select(event,time,delta)
-all_events$population_size <- cumsum(ifelse(all_events$event == "infection", +1, -1) * all_events$delta)
+all_events$population_size <- cumsum(ifelse(all_events$event == "infection", +1, -1) * all_events$delta) + 1
 final_prevalence <- tail(all_events$population_size, 1)
 
-nb_params <- readLines("out/prevalence-distribution.txt") %>%
+
+
+
+
+raw_newick_string <- readLines("out/reconstructed-newick-tree.txt")
+smaller_newick_string <- gsub(pattern = "([0-9]+&)*[0-9]+:",
+                              replacement = "x:",
+                              x = raw_newick_string)
+tree <- ape::read.tree(text=sprintf("(%s);", smaller_newick_string))
+tree_ltt <- as.data.frame(ltt.plot.coords(tree))
+## tree_ltt$time <- tree_ltt$time + (config$simulationParameters[[4]] %>% map(head(1)) %>% unlist %>% keep(~ .x < config$simulationDuration) %>% max)
+tree_ltt$time <- tree_ltt$time + (all_events %>% filter(event == "sampling" | event == "catastrophe") %>% select(time) %>% max)
+
+
+
+
+
+
+partial_results <- read.csv("out/partial-evaluations.csv",
+                            header = FALSE,
+                            stringsAsFactors = FALSE)
+
+nb_params <- partial_results$V2 %>%
     strsplit(split = " ") %>%
-    unlist %>%
-    tail(2) %>%
-    as.double %>%
-    as.list %>%
-    set_names(c("size", "prob"))
+    map(~ set_names(as.list(as.double(tail(.x, 2))), c("size", "prob")))
 
-prev_vals <- 0:100
-prev_post_probs <- dnbinom(x = prev_vals, size = nb_params$size, prob = nb_params$prob)
+prev_bounds <- function(x) {
+    ## We use 1 - x$prob here because R uses a different parameterisation.
+    set_names(as.list(qnbinom(p = c(0.005,0.5,0.995), size = x$size, prob = 1-x$prob)), c("lower","mid","upper"))
+}
 
-plot_df <- data.frame(prevalence = prev_vals,
-                      posterior = prev_post_probs)
+sim_obs_times <- read.csv("out/simulated-observations.csv", header = FALSE)$V1 %>% cumsum
 
-prev_fig <- ggplot(plot_df, aes(x = prevalence, y = posterior)) +
-    geom_line() +
-    geom_vline(xintercept = final_prevalence, linetype = "dashed") +
+plot_df <- map(nb_params, prev_bounds) %>% rev %>% tail(-1) %>% bind_rows
+plot_df$time <- sim_obs_times
+plot_df <- inner_join(plot_df, tree_ltt, by = "time")
+
+
+prev_fig <- ggplot(plot_df, aes(x = time)) +
+    geom_ribbon(mapping = aes(ymin = lower + N, ymax = upper + N), alpha = 0.2) +
+    geom_line(mapping = aes(y = mid + N)) +
+    geom_line(data = all_events, mapping = aes(y = population_size), colour = "red") +
     theme_classic()
+print(prev_fig)
 
 ggsave("out/prevalence-profiles.png", prev_fig)
