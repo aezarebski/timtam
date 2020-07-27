89cd5aa7b3fdbb1bd6ee22ff686f1090638c8ddf
Alex Zarebski <aezarebski@gmail.com>
2020-07-27 16:08:00 +0100

    current json config

--- examples/simulation-study-time-series/src/make-json-config.R
+++ examples/simulation-study-time-series/src/make-json-config.R
@@ -4,45 +4,42 @@ library(jsonlite)
 output_file <- "ts-config.json"
 
 
-simulation_duration <- 18
+simulation_duration <- 17
 
-## Toggling the birth rate between 2.5 and 2.6 will break the likelihood for an
-## unknown reason.
-## birth_rate <- 2.5
-birth_rate <- 2.6
+birth_rate <- 1.7
+death_rate <- 0.30
+sampling_rate <- 0.3
+occurrence_rate <- 0.3
 
-death_rate <- 0.70
-
-disaster_times <- seq(from = 2, to = simulation_duration, by = 1)
+disaster_times <- seq(from = 2, to = simulation_duration, by = 1.5)
 num_disasters <- length(disaster_times)
-disaster_probs <- rep(0.5, num_disasters)
+disaster_probs <- rep(0.25, num_disasters)
 disaster_params <- map2(disaster_times, disaster_probs, list)
 
 
 catastrophe_times <- disaster_times + 0.5
 num_catastrophes <- length(catastrophe_times)
-catastrophe_probs <- seq(from = 0.5, to = 0.8, length = num_catastrophes)
+catastrophe_probs <- seq(from = 0.25, to = 0.25, length = num_catastrophes)
 catastrophe_params <- map2(catastrophe_times, catastrophe_probs, list)
 
-sim_params <- list(birth_rate, death_rate, 0.0, catastrophe_params, 0.0, disaster_params)
+sim_params <- list(birth_rate, death_rate, sampling_rate, catastrophe_params, occurrence_rate, disaster_params)
 
-num_eval_steps <- 100
-eval_lambda <- seq(from = 1.5, to = 4.5, length = num_eval_steps)
-eval_params_lambda <- map(.x = eval_lambda, .f = ~ list(.x, death_rate, 0.0, catastrophe_params, 0.0, disaster_params))
-eval_mu <- seq(from = 0.1, to = 1.3, length = num_eval_steps)
-eval_params_mu <- map(.x = eval_mu, .f = ~ list(birth_rate, .x, 0.0, catastrophe_params, 0.0, disaster_params))
+num_eval_steps <- 100 # the number of mesh points for plotting
+eval_lambda <- seq(from = 1.0, to = 3.0, length = num_eval_steps)
+eval_params_lambda <- map(.x = eval_lambda, .f = ~ list(.x, death_rate, sampling_rate, catastrophe_params, occurrence_rate, disaster_params))
+eval_mu <- seq(from = 0.05, to = 0.5, length = num_eval_steps)
+eval_params_mu <- map(.x = eval_mu, .f = ~ list(birth_rate, .x, sampling_rate, catastrophe_params, occurrence_rate, disaster_params))
 eval_params <- c(eval_params_lambda, eval_params_mu)
 
 
 result <- list(
     simulatedEventsOutputCsv = "out/all-simulated-events.csv",
-    simulationParameters = sim_params, # [3.5, 0.1, 0.0, [[1.5,0.2],[2.4,0.3],[3.4,0.3]], 0.0, [[1.0,0.1]]],
+    simulationParameters = sim_params,
     simulationDuration = simulation_duration + 1e-5,
     reconstructedTreeOutputFiles = c("out/reconstructed-newick-tree.txt","out/reconstructed-newick-metadata.csv"),
     observationsOutputCsv = "out/simulated-observations.csv",
     evaluationParameters = eval_params,
-    llhdOutputCsv = "out/llhd-evaluations.csv",
-    prevalenceDistributionTxt = "out/prevalence-distribution.txt"
+    llhdOutputCsv = "out/llhd-evaluations.csv"
 )
 
 write_json(result, output_file, pretty = FALSE, auto_unbox = TRUE, digits = 7)
@@ -52,6 +49,10 @@ eval_df <- data.frame(parameter = rep(c("lambda", "mu"), each = num_eval_steps),
 
 write.table(x = eval_df, file = "out/evaluation-parameters.csv", row.names = FALSE)
 
-true_parameters <- data.frame(parameter = c("lambda", "mu"), value = c(birth_rate,death_rate))
-
-write.table(x = true_parameters, file = "out/true-parameters.csv", row.names = FALSE)
+## save a copy of the true parameters so they can be read out later rather than
+## hardcoded.
+true_parameters <- data.frame(parameter = c("lambda", "mu"),
+                              value = c(birth_rate,death_rate))
+write.table(x = true_parameters,
+            file = "out/true-parameters.csv",
+            row.names = FALSE)
