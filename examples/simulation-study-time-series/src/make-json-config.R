135e4e36f259571a0172a46cd4e5f285ed2f6128
Alex Zarebski <aezarebski@gmail.com>
2020-07-28 10:36:29 +0100

    use more sensible parameters in the simulation stidy

--- examples/simulation-study-time-series/src/make-json-config.R
+++ examples/simulation-study-time-series/src/make-json-config.R
@@ -4,22 +4,21 @@ library(jsonlite)
 output_file <- "ts-config.json"
 
 
-simulation_duration <- 17
+simulation_duration <- 15
 
-birth_rate <- 1.7
-death_rate <- 0.30
-sampling_rate <- 0.3
-occurrence_rate <- 0.3
+birth_rate <- 1.3
+death_rate <- 0.50
+sampling_rate <- 0.2
+occurrence_rate <- 0.2
 
 disaster_times <- seq(from = 2, to = simulation_duration, by = 1.5)
 num_disasters <- length(disaster_times)
-disaster_probs <- rep(0.25, num_disasters)
+disaster_probs <- rep(0.15, num_disasters)
 disaster_params <- map2(disaster_times, disaster_probs, list)
-
 
 catastrophe_times <- disaster_times + 0.5
 num_catastrophes <- length(catastrophe_times)
-catastrophe_probs <- seq(from = 0.25, to = 0.25, length = num_catastrophes)
+catastrophe_probs <- seq(from = 0.15, to = 0.25, length = num_catastrophes)
 catastrophe_params <- map2(catastrophe_times, catastrophe_probs, list)
 
 sim_params <- list(birth_rate, death_rate, sampling_rate, catastrophe_params, occurrence_rate, disaster_params)
@@ -44,9 +43,10 @@ result <- list(
 
 write_json(result, output_file, pretty = FALSE, auto_unbox = TRUE, digits = 7)
 
+## save a copy of the "evaluation paramters" to make it easier to plot the
+## profiles later.
 eval_df <- data.frame(parameter = rep(c("lambda", "mu"), each = num_eval_steps),
                       value = c(eval_lambda, eval_mu))
-
 write.table(x = eval_df, file = "out/evaluation-parameters.csv", row.names = FALSE)
 
 ## save a copy of the true parameters so they can be read out later rather than
