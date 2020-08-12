2d554095c3ffb8aa35e8c93cfc1be3ee0a90e40f
Alex Zarebski <aezarebski@gmail.com>
2020-08-12 14:56:27 +0100

    define scaffolding for the aggregation function

--- src/BDSCOD/Aggregation.hs
+++ src/BDSCOD/Aggregation.hs
@@ -1,13 +1,50 @@
 
-module BDSCOD.Aggregation where
+module BDSCOD.Aggregation
+  ( aggregateUnscheduledObservations
+  ) where
 
 import BDSCOD.Types
+import Epidemic.Types.Parameter
 
--- | If all of the observations are unscheduled and occur before the last
--- aggregation time this will return the aggregated observations.
+-- | The absolute time and the observation at that time.
+type AnnotatedObservation = (Time, Observation)
+
+-- | The observations where unscheduled observations have been rounded up to the
+-- first aggregation time after they occurred. If there are unscheduled
+-- observations after the final aggregation time the resut is nothing.
 aggregateUnscheduledObservations :: AggregationTimes
                                  -> [Observation]
                                  -> Maybe AggregatedObservations
-aggregateUnscheduledObservations = undefined
+aggregateUnscheduledObservations aggTimes@(AggregationTimes ats) obs =
+  do
+    let annObs = annotatedObs obs
+    aggAnnObs <- aggregatedObs ats ([],annObs)
+    let aggObs = withCorrectedDelays aggAnnObs
+    return $ AggregatedObservations aggTimes aggObs
 
+-- | The observations with the delays corrected by the annotations.
+withCorrectedDelays :: [AnnotatedObservation] -> [Observation]
+withCorrectedDelays annObs =
+  let absTimes = map fst annObs
+      obs = map snd annObs
+      collectDelay (currTime,_) nextTime = (nextTime,nextTime-currTime)
+      delays = map snd . tail . scanl collectDelay (0,0) $ absTimes
+    in zipWith updateDelay obs delays
+
+
+-- | Aggregated all unscheduled observations into scheduled observations at the
+-- given aggregation times and return those and any unscheduled observations
+-- that occurred after the last aggregation time.
+aggregatedObs :: [Time] -- ^ the aggregation times
+              -> ([AnnotatedObservation],[AnnotatedObservation])
+              -> Maybe [AnnotatedObservation]
+aggregatedObs = undefined
+
+
+-- | Add the absolute time of each observation as an annotation to the
+-- observation.
+annotatedObs :: [Observation] -> [AnnotatedObservation]
+annotatedObs obs =
+  let absTimes = tail . scanl (+) 0 . map fst $ obs
+  in zip absTimes obs
 
