ea6559dc6a08ec8271dafa5747ed97d14aad5495
Alex Zarebski <aezarebski@gmail.com>
2020-11-13 13:40:08 +0000

    hopefully fix the aggregation module

--- src/BDSCOD/Aggregation.hs
+++ src/BDSCOD/Aggregation.hs
@@ -6,8 +6,6 @@ module BDSCOD.Aggregation
   , AggregationTimes()
   , pattern AggTimes
   , maybeAggregationTimes
-  ,extractFirstAggregationTime
-  ,nullAggregationTimes
   , AggregatedObservations(..)
   ) where
 
@@ -60,20 +58,6 @@ data AggregatedObservations =
   AggregatedObservations AggregationTimes [Observation]
   deriving (Show, Eq)
 
--- | Return the first aggregation time and the type of unscheduled observation
--- to aggregate in it, along with a new set of aggregation times with the first
--- one removed. Since there is a smart constructor, we assume that the initial
--- @AggregationTimes@ value is valid.
-extractFirstAggregationTime :: AggregationTimes -> Maybe ((Time,ObservedEvent),AggregationTimes)
-extractFirstAggregationTime (AggregationTimes_ ts) = case ts of
-  [] -> Nothing
-  [t] -> Just (t, AggregationTimes_ [])
-  (t:ts') -> Just (t, AggregationTimes_ ts')
-
--- | Predicate for there being no aggregation times in the @AggregationTimes@.
-nullAggregationTimes :: AggregationTimes -> Bool
-nullAggregationTimes (AggregationTimes_ ts) = null ts
-
 -- | The absolute time and the observation at that time. Recall that
 -- `Observation` pairs only contain the time delay since the previous observable
 -- event, not their absolute time.
@@ -88,74 +72,52 @@ type AnnotatedObservation = (Time, Observation)
 --
 aggregateUnscheduledObservations :: AggregationTimes
                                  -> [Observation]
-                                 -> Maybe AggregatedObservations
+                                 -> AggregatedObservations
 aggregateUnscheduledObservations aggTimes obs =
-  do
-    let annObs = annotatedObs obs
-    aggAnnObs <- aggregatedObs aggTimes annObs
-    let aggObs = withCorrectedDelays aggAnnObs
-    return $ AggregatedObservations aggTimes aggObs
+  let annObs = annotatedObs obs
+      aggAnnObs = aggregatedObs aggTimes annObs
+      aggObs = withDelaysNoAnnotations aggAnnObs
+    in AggregatedObservations aggTimes aggObs
 
--- | The observations with the delays corrected by the annotations.
-withCorrectedDelays :: [AnnotatedObservation] -> [Observation]
-withCorrectedDelays annObs =
+-- | The observations with the new delays computed from the annotations which
+-- have been removed.
+withDelaysNoAnnotations :: [AnnotatedObservation] -> [Observation]
+withDelaysNoAnnotations annObs =
   let absTimes = map fst annObs
       obs = map snd annObs
-      collectDelay (currTime,_) nextTime = (nextTime,nextTime-currTime)
+      collectDelay (currTime,_) newTime = (newTime,newTime-currTime)
       delays = map snd . tail . scanl collectDelay (0,0) $ absTimes
     in zipWith updateDelay obs delays
 
-
--- | Aggregated all unscheduled observations into scheduled observations at the
--- given aggregation times and return those and any unscheduled observations
--- that occurred after the last aggregation time.
---
--- __NOTE__ that if there are any unscheduled observations remaining after the
--- last aggregation time then this will fail and return `Nothing`.
---
-aggregatedObs :: AggregationTimes
-              -> [AnnotatedObservation]
-              -> Maybe [AnnotatedObservation]
-aggregatedObs aggTimes annObs
-  | nullAggregationTimes aggTimes =
-    if or [isUnscheduledSequenced o || isOccurrence o | (_, o) <- annObs]
-      then Nothing
-      else Just annObs
-  | otherwise = do
-    (aggTime, remAggTimes) <- extractFirstAggregationTime aggTimes
-    partiallyAggAnnObs <- _aggregatedObs annObs aggTime
-    aggregatedObs remAggTimes partiallyAggAnnObs
-
--- | Join all unscheduled observations before a given time into a single
--- scheduled observation. This is in the maybe monad because aggregation is a
--- partial function.
-_aggregatedObs :: [AnnotatedObservation]
-               -> Time
-               -> Maybe [AnnotatedObservation]
-_aggregatedObs annObs aggTime =
-  let beforeObs = filter (\(absTime, obs) -> absTime <= aggTime && (not . isUnscheduledSequenced) obs) annObs
-      needAggObs = filter (\(absTime, obs) -> absTime <= aggTime && isUnscheduledSequenced obs) annObs
-      afterObs = filter (\(absTime, _) -> absTime > aggTime) annObs
-      in do aggObs <- _aggregateSamples needAggObs aggTime
-            return $ mconcat [beforeObs,[aggObs],afterObs]
-
--- | Join a list of observed samples into a single catastrophe at the given
--- time. The delay on the returned catastrophe is `-Infinity`to make it clear
--- that this is not a real value since the true value depends on the preceeding
--- observation. If there are events that are not samples then this will return
--- nothing.
---
-_aggregateSamples :: [AnnotatedObservation] -> Time -> Maybe AnnotatedObservation
-_aggregateSamples annSamples aggTime =
-  if all (isUnscheduledSequenced . snd) annSamples
-    then Just
-           (aggTime, (-1/0, OCatastrophe (fromIntegral $ length annSamples)))
-    else Nothing
-
 -- | Add the absolute time of each observation as an annotation to the
--- observation.
+-- observation assuming that the first delay is since the origin time, zero.
 annotatedObs :: [Observation] -> [AnnotatedObservation]
 annotatedObs obs =
-  let absTimes = tail . scanl (+) 0 . map fst $ obs
+  let delayVals = map fst obs
+      absTimes = (tail . scanl (+) 0) delayVals
   in zip absTimes obs
 
+-- | Aggregate all annotated unscheduled observations into annotated scheduled
+-- observations at the given aggregation times. If there are annotated
+-- unscheduled observation occurs after the last aggregation time then it is
+-- left as it is.
+--
+-- NOTE that this function will return an undefined value if the type of
+-- unscheduled event to aggregate does not make sense. The variable @bogusDelay@
+-- is because without already having computed the result the correct delay
+-- values cannot be computed.
+aggregatedObs :: AggregationTimes
+              -> [AnnotatedObservation]
+              -> [AnnotatedObservation]
+aggregatedObs (AggregationTimes_ []) annObs = annObs
+aggregatedObs (AggregationTimes_ ((aggTime,obsType):aTs)) annObs =
+  let earlierAnnObs = filter (\(absT,(_,o)) -> absT < aggTime && o /= obsType) annObs
+      numAggregated = fromIntegral . length $ filter (\(absT,(_,o)) -> absT < aggTime && o == obsType) annObs
+      bogusDelay = -1/0
+      aggAnnObs
+        | obsType == ObsUnscheduledSequenced = (aggTime,(bogusDelay,OCatastrophe numAggregated))
+        | obsType == OOccurrence = (aggTime,(bogusDelay,ODisaster numAggregated))
+        | otherwise = undefined
+      laterAnnObs = filter (\(absT,_) -> absT > aggTime) annObs
+      annObs' = earlierAnnObs ++ [aggAnnObs] ++ laterAnnObs
+    in aggregatedObs (AggregationTimes_ aTs) annObs'
