71313bf54d2fe14084e050b7ce6794681b6f9e03
Alex Zarebski <aezarebski@gmail.com>
2021-02-03 13:34:41 +0000

    demonstrate how to parse the data out of the json

--- examples/prevalence-ci-calibration/src/plotter.R
+++ examples/prevalence-ci-calibration/src/plotter.R
@@ -7,8 +7,32 @@ library(reshape2)
 library(jsonlite)
 
 
+vis_data <- read_json("demo.json")
+
+r_naught_and_prevalence_record <- function(sim_result) {
+  sim_seed <- sim_result$simulationSeed
+  r_naught <- sim_result$regularParameterEstimates %>%
+    keep(~ .x$name == "rNaught") %>%
+    flatten()
+  data.frame(
+    seed = sim_seed,
+    r_naught_est = r_naught$estimate,
+    r_naught_lower = r_naught$credibleInterval[[1]],
+    r_naught_upper = r_naught$credibleInterval[[2]]
+  )
+}
+
+## This is the data frame that produces the figure in the main text.
+r_naught_and_prevalence_df <- map(vis_data$simulationResults,
+                                  r_naught_and_prevalence_record) %>%
+  bind_rows
+
+
+
+## =============================================================================
+
 make_figures <- function(vis_data) {
- 
+
 }
 
 main <- function(args) {
