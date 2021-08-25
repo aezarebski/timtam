a8638864a0d497e79916d64e55b5a8c62033fac3
Alex Zarebski <aezarebski@gmail.com>
2021-08-25 14:45:59 +0100

    make it easier to adjust the simulation size and organise helper scripts

--- examples/prevalence-ci-calibration/clean.sh
+++ examples/prevalence-ci-calibration/clean.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 rm -f -r out/*
+
+rm timtam-figure-1.pdf
+rm timtam-figure-s7a.pdf
+rm timtam-figure-s7b.pdf
+rm timtam-figure-s8a.pdf
+rm timtam-figure-s8b.pdf
+rm timtam-report.html
--- examples/prevalence-ci-calibration/run.sh
+++ examples/prevalence-ci-calibration/run.sh
@@ -4,23 +4,29 @@
 #
 # This must be run with bash!!!
 
-NUM_SEEDS=100
+NUM_SEEDS=30
+PAR_SIZE=15                     # number of processes to run in parallel.
 VIS_JSON=out/vis-data.json
 
+# the arguments specify the lower and upper limit on the range of seeds to use.
 Rscript src/generate-config-files.R 1 $NUM_SEEDS
 
-for ix in 00 10 20 30 40 50 60 70 80 90;
+IX=1
+OUTER_LOOP_LIMIT="$(($NUM_SEEDS-$PAR_SIZE))"
+while [ $IX -le $OUTER_LOOP_LIMIT ]
 do
-    pids=()
-
-    for jx in {1..10};
+    PIDS=()
+    PAR_LIMIT="$(($IX+$PAR_SIZE))"
+    while [ $IX -le $PAR_LIMIT ]
     do
-        kx=$(($ix + $jx))
-        stack exec -- simulation-study-aggregated-observations out/seed-$kx/config-$kx.json && echo "Finished $kx" & pids+=($!)
+        PADDED_IX=$(printf "%03d" $IX)
+        echo "Starting $IX"
+        stack exec -- simulation-study-aggregated-observations out/seed-$PADDED_IX/config-$PADDED_IX.json && echo "Finished $IX" & pids+=($!)
+        ((IX++))
     done
-
     wait "${pids[@]}"
 done
+
 
 Rscript src/summarize-results.R $NUM_SEEDS $VIS_JSON
 Rscript src/plotter.R $VIS_JSON
