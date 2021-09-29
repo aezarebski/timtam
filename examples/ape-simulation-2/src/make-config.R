2adef26bf5044fbc8d9356dca1819f345777e22e
Alex Zarebski <aezarebski@gmail.com>
2021-09-29 18:53:59 +0100

    run analysis of aggregated data too

--- examples/ape-simulation-2/src/make-config.R
+++ examples/ape-simulation-2/src/make-config.R
@@ -30,72 +30,90 @@ parser$add_argument(
          help = "Filepath for output data"
        )
 parser$add_argument(
+         "--parameterisation",
+         type = "character",
+         help = "Parameterisation of the model"
+       )
+parser$add_argument(
          "--unscheduled-data",
          action = "store_true",
          default = FALSE,
          help = "Do not modify the unscheduled data"
        )
 parser$add_argument(
-         "--aggregate-sequenced-from-to-by",
-         type = "double",
-         help = "Aggregate sequenced samples using these three values to construct a mesh"
-       )
-parser$add_argument(
-         "--aggregate-unsequenced-from-to-by",
-         type = "double",
-         help = "Aggregate unsequenced samples using these three values to construct a mesh"
+         "--num-mcmc-samples",
+         type = "integer",
+         help = "The number of posterior samples to generate via MCMC."
        )
 
-observation <- function(d, e) {
-  list(
-    d,
-    list(
-      tag = switch(
-        e,
-        birth = "OBirth",
-        occurrence = "OOccurrence",
-        sampling = "ObsUnscheduledSequenced"
-      )
-    )
+fix_from_to_by <- function(from_to_by_string) {
+  if (from_to_by_string != "") {
+    tmp <- as.numeric(unlist(strsplit(from_to_by_string, split = " ")))
+    return(seq(from = tmp[1], to = tmp[2], by = tmp[3]))
+  } else {
+    return(NULL)
+  }
+}
+
+observation <- function(d, e, s) {
+  switch(
+    e,
+    birth = list(d, list(tag = "OBirth")),
+    occurrence = list(d, list(tag = "OOccurrence")),
+    sampling = list(d, list(tag = "ObsUnscheduledSequenced")),
+    rho = list(d, list(tag = "OCatastrophe", contents = s)),
+    nu = list(d, list(tag = "ODisaster", contents = s))
   )
 }
 
+## Extension of purrr::map2 to handle three arguments.
+map3 <- function(xs, ys, zs, f) {
+  lapply(seq_along(xs), function(ix) f(xs[ix], ys[ix], zs[ix]))
+}
 
 main <- function(args) {
-  if (args$verbose) {
-    cat("reading parameters from", args$parameters, "\n")
-  }
   set.seed(args$seed)
   if (file.exists(args$input)) {
     ## the simulation events are saved in order of type rather than time so we
     ## need to re-order them.
     sim_events <- read.csv(args$input)
     sim_events <- sim_events[order(sim_events$time), ]
-    stopifnot(all(sim_events$events != "rho"))
-
+    ## Because the MCMC works on the sums of delays we need to adjust for the
+    ## TMRCA offset when we compute the rho and nu times from the simulation
+    ## events.
+    tmrca_offset <- sim_events[sim_events$event == "origin", ]$time
+    rho_times <- sim_events[sim_events$event == "rho", ]$time - tmrca_offset
+    nu_times <- sim_events[sim_events$event == "nu", ]$time - tmrca_offset
     delays <- diff(sim_events$time)
     events <- tail(sim_events$event, -1)
+    if (is.element("size", names(sim_events))) {
+      sizes <- tail(sim_events$size, -1)
+    } else {
+      sizes <- rep(NA, length(events))
+    }
   } else {
     stop("Could not find given input file: ", args$input)
   }
 
   true_params <- jsonlite::read_json("../example-parameters.json")
 
-  observations_list <- purrr::map2(delays, events, observation)
+  observations_list <- map3(delays, events, sizes, observation)
 
+  ## TODO Set this up so that it generates a random initial condition sensibly...
   mcmc_init <- c(0.228, 0.048, 0.026)
-  mcmc_parameterisation <- "identity-muKnown-lambda-psi-noRho-omega-noNu"
 
   mcmc_input <- list(
     mcmcObservations = observations_list,
-    mcmcNumSamples = 2e5,
+    mcmcNumSamples = args$num_mcmc_samples,
     mcmcSampleCSV= paste0(c(dirname(args$output), "posterior-samples.csv"), collapse = "/"),
     mcmcRecordFinalPrevalence = TRUE,
     mcmcStepSD  = 1e-3,
     mcmcInit   = mcmc_init,
     mcmcSeed  = c(1, 2),
-    mcmcParameterisation = mcmc_parameterisation,
+    mcmcParameterisation = args$parameterisation,
     mcmcKnownMu = true_params$deathRate,
+    mcmcRhoTimes = rho_times,
+    mcmcNuTimes = nu_times,
     mcmcPrior = "foobar"
   )
 
@@ -110,5 +128,15 @@ main <- function(args) {
 
 if (!interactive()) {
   args <- parser$parse_args()
+  main(args)
+} else {
+  args <- list(
+    verbose = TRUE,
+    seed = 1,
+    input = "out/ape-sim-aggregated-event-times.csv",
+    output = "out/aggregated-data/mcmc-app-config.json",
+    parameterisation = "identity-muKnown-lambda-psi-noRho-omega-noNu",
+    unscheduled_data = FALSE
+  )
   main(args)
 }
--- examples/ape-simulation-2/src/make-viewer.R
+++ examples/ape-simulation-2/src/make-viewer.R
@@ -2,6 +2,8 @@ library(htmltools)
 library(base64enc)
 library(purrr)
 
+out_html <- "index.html"
+
 #' An HTML tag encoding an image stored in a PNG.
 #'
 #' This uses the \code{base64enc} and \code{htmltools} packages.
@@ -34,14 +36,23 @@ traceplot_div <- function(fps) {
 }
 
 marginal_div <- function(fp1, fp2, fp3) {
-  tags$div(
-    tags$h5("Posterior marginals"),
-    png_as_img(filepath = fp1, style = "width: 900px;"),
-    tags$h5("Reproduction number"),
-    png_as_img(filepath = fp2, style = "width: 900px;"),
-    tags$h5("Prevalence"),
-    png_as_img(filepath = fp3, style = "width: 900px;")
-  )
+  if (!is.null(fp2)) {
+    tags$div(
+           tags$h5("Posterior marginals"),
+           png_as_img(filepath = fp1, style = "width: 900px;"),
+           tags$h5("Reproduction number"),
+           png_as_img(filepath = fp2, style = "width: 900px;"),
+           tags$h5("Prevalence"),
+           png_as_img(filepath = fp3, style = "width: 900px;")
+         )
+  } else {
+    tags$div(
+           tags$h5("Posterior marginals"),
+           png_as_img(filepath = fp1, style = "width: 900px;"),
+           tags$h5("Prevalence"),
+           png_as_img(filepath = fp3, style = "width: 900px;")
+         )
+  }
 }
 
 splom_div <- function(fp) {
@@ -54,16 +65,14 @@ splom_div <- function(fp) {
 #' Construct the actual HTML page
 #' =============================================================================
 
-unscheduled_data_traceplot_fps <- list.files(
-  path = "out/unscheduled-data/",
-  pattern = "*traceplot*",
-  full.names = TRUE
-)
-
 
 html_body <-
   tags$body(
-    tags$h1("ape simulation example II"),
+         tags$h1("ape simulation example II"),
+         tags$h3("Simulated data"),
+         png_as_img(filepath = "out/ape-simulation-figure.png", style = "width: 900px;"),
+         tags$h3("Aggregated simulated data"),
+         png_as_img(filepath = "out/ape-simulation-figure-aggregated.png", style = "width: 900px;"),
     tags$div(
       tags$h3("Unscheduled data"),
       tags$div(
@@ -71,9 +80,32 @@ html_body <-
                           "out/unscheduled-data/r-naught.png",
                           "out/unscheduled-data/prevalence.png"),
              splom_div("out/unscheduled-data/splom.png"),
-        traceplot_div(unscheduled_data_traceplot_fps)
+             traceplot_div(list.files(
+               path = "out/unscheduled-data/",
+               pattern = "*traceplot*",
+               full.names = TRUE
+))
       )
-    )
+      ),
+    tags$div(
+           tags$h3("Aggregated data"),
+           tags$div(
+                  marginal_div("out/aggregated-data/marginal-distributions.png",
+                               NULL,
+                               "out/aggregated-data/prevalence.png"),
+                  splom_div("out/aggregated-data/splom.png"),
+                  traceplot_div(list.files(
+                    path = "out/aggregated-data/",
+                    pattern = "*traceplot*",
+                    full.names = TRUE
+))
+                )
+         )
   )
 
-save_html(html_body, file = "index.html")
+save_html(html_body, file = out_html)
+cat(
+  "Results can be viewed by pointing browser to\n\t",
+  paste("file:/", getwd(), out_html, sep = "/"),
+  "\n"
+)
