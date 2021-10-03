f038204797b197c48e2c84f49bf9e478d7e25775
Alex Zarebski <aezarebski@gmail.com>
2021-10-03 17:27:29 +0100

    fix mcmc runner so it takes a cla and loops the correct number of times

--- examples/ci-calibration-2/src/run-mcmc.sh
+++ examples/ci-calibration-2/src/run-mcmc.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-NUM_SEEDS=50                    # total number of replicates
-PAR_SIZE=15                     # number of processes to run in parallel.
+NUM_SEEDS=$1                    # total number of replicates
+PAR_SIZE=10                     # number of processes to run in parallel.
 
 AGG_JSON=mcmc-app-config-aggregated.json
 SEQ_JSON=mcmc-app-config.json
 
 IX=1
-OUTER_LOOP_LIMIT="$(($NUM_SEEDS-$PAR_SIZE))"
+OUTER_LOOP_LIMIT="$(($NUM_SEEDS-$PAR_SIZE+1))"
 while [ $IX -le $OUTER_LOOP_LIMIT ]
 do
+    echo "-----"
     PIDS=()
     PAR_LIMIT="$(($IX+$PAR_SIZE))"
-    while [ $IX -le $PAR_LIMIT ]
+    while [ $IX -lt $PAR_LIMIT ]
     do
         PADDED_IX=$(printf "%03d" $IX)
         echo "Starting $IX"
